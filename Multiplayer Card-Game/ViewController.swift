ee5f530a4bd4ecd1f03e5347f98b10cb9d2c99d4
tushar40 <tushar@hotcocoasoftware.com>
2019-12-27 13:55:19 +0530

    Initial Commit

---
+++ Multiplayer Card-Game/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Multiplayer Card-Game
+//
+//  Created by Tushar Gusain on 27/12/19.
+//  Copyright © 2019 Hot Cocoa Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Multiplayer Card-GameTests/Multiplayer_Card_GameTests.swift
@@ -0,0 +1,34 @@
+//
+//  Multiplayer_Card_GameTests.swift
+//  Multiplayer Card-GameTests
+//
+//  Created by Tushar Gusain on 27/12/19.
+//  Copyright © 2019 Hot Cocoa Software. All rights reserved.
+//
+
+import XCTest
+@testable import Multiplayer_Card_Game
+
+class Multiplayer_Card_GameTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
